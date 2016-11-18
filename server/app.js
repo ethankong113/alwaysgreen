be088155082bea0f0261a0832d585cc226e3afab
ethankong113 <kongwan113@gmail.com>
2016-11-18 12:16:07 -0800

    changing backend from es6 to es5

--- server/app.js
+++ server/app.js
@@ -1,9 +1,9 @@
-let http = require('http');
-let express = require('express');
-let path = require('path');
-let app = express();
-let fs = require('fs');
-let readCSV = require('./spider.js');
+var http = require('http');
+var express = require('express');
+var path = require('path');
+var app = express();
+var fs = require('fs');
+var readCSV = require('./spider.js');
 
 app.use(express.static(path.join(__dirname, '../client/assets')));
 
@@ -12,11 +12,11 @@ app.get('/', (req, res)=> {
 });
 
 app.get('/:ticker/prices', (req, res) => {
-  let ticker = req.params.ticker;
+  var ticker = req.params.ticker;
   fs.readFile(path.join(__dirname, `../data/${ticker}/prices.json`), 'utf8', (err, data) => {
     if (err) throw err;
-    let raw = JSON.parse(data).dataset.data;
-    let info = raw.map((datum)=> {
+    var raw = JSON.parse(data).dataset.data;
+    var info = raw.map((datum)=> {
       return {
         date: datum[0],
         price: datum[11],
@@ -28,7 +28,7 @@ app.get('/:ticker/prices', (req, res) => {
 });
 
 app.get('/:ticker/fundamentals', (req, res) => {
-  let ticker = req.params.ticker;
+  var ticker = req.params.ticker;
   readCSV(ticker, (data) => {res.json(data);});
 });
 
--- server/spider.js
+++ server/spider.js
@@ -3,37 +3,37 @@ var path = require('path');
 var { merge } = require('lodash');
 
 function readCSV(ticker, callback) {
-  let pathQ = path.join(__dirname, `../data/${ticker}/dataByQ.csv`);
-  let pathY = path.join(__dirname, `../data/${ticker}/dataByY.csv`);
+  var pathQ = path.join(__dirname, `../data/${ticker}/dataByQ.csv`);
+  var pathY = path.join(__dirname, `../data/${ticker}/dataByY.csv`);
   return (fs.readFile(pathQ, 'utf8', (err, data) => {
-    let lines = data.split("\n");
+    var lines = data.split("\n");
     lines = lines.slice(0,lines.length-1);
-    let dates = lines[0].split(',').map((date) => {
+    var dates = lines[0].split(',').map((date) => {
       return date.replace(/[ \t]/g, "");
     });
-    let output = {};
+    var output = {};
     lines.slice(1).forEach((line) => {
-      let values = line.split(",").map((value) => {
+      var values = line.split(",").map((value) => {
         return value.replace(/[ \t]/g, "");
       });
       output[values[0]] = [];
-      for (let i = 1; i < values.length; i++) {
+      for (var i = 1; i < values.length; i++) {
         output[values[0]].push({date: dates[i], value: parseFloat(values[i])});
       }
     });
     fs.readFile(pathY, 'utf8', (err2, data2) => {
-      let lines2 = data2.split("\n");
+      var lines2 = data2.split("\n");
       lines2 = lines2.slice(0,lines2.length-1);
-      let dates2 = lines2[0].split(',').map((date) => {
+      var dates2 = lines2[0].split(',').map((date) => {
         return date.replace(/[ \t]/g, "");
       });
-      let output2 = {};
+      var output2 = {};
       lines2.slice(1).forEach((line) => {
-        let values = line.split(",").map((value) => {
+        var values = line.split(",").map((value) => {
           return value.replace(/[ \t]/g, "");
         });
         output2[values[0]] = [];
-        for (let i = 1; i < values.length; i++) {
+        for (var i = 1; i < values.length; i++) {
           output2[values[0]].push({date: dates2[i], value: parseFloat(values[i])});
         }
 
